b39c8153af60a1d006ccd265b46bd515bb77f6f2
L-F-F-F <33219996+L-F-F-F@users.noreply.github.com>
2018-10-28 17:56:41 +0800

    part2

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # yolo-v3-pytorch
 ---
-AlexeyAB的[yolo-v3](https://github.com/AlexeyAB/darknet)基于C语言的darknet框架。yolo-v3的pytorch搭建英文教程[How to implement a YOLO (v3) object detector from scratch in PyTorch](https://blog.paperspace.com/how-to-implement-a-yolo-object-detector-in-pytorch/)，原作者讲的很详细，网上其他部分教程都是基于这篇博客改写的，教程分为五部分：
+AlexeyAB的[yolo-v3](https://github.com/AlexeyAB/darknet)基于C语言的darknet框架。yolo-v3的pytorch搭建英文教程[How to implement a YOLO (v3) object detector from scratch in PyTorch](https://blog.paperspace.com/how-to-implement-a-yolo-object-detector-in-pytorch/)，原作者讲的很详细，网上其他部分教程都是基于这篇博客改写的（有些地方英文原版比中文翻译版好理解），教程分为五部分：
 
 	1、YOLO 的工作原理
 	2、创建 YOLO 网络层级
 	3、实现网络的前向传播
 	4、objectness 置信度阈值和非极大值抑制
 	5、设计输入和输出管道
-[机器之心](https://www.jiqizhixin.com/users/7f316f0c-8f72-4231-bb30-0eb1dd5a5660)有两篇不错的中文翻译教程：[从零开始PyTorch项目：YOLO v3目标检测实现](https://www.jiqizhixin.com/articles/2018-04-23-3)，和[下篇](https://www.jiqizhixin.com/articles/042602)。github上源码[ayooshkathuria/pytorch-yolo-v3](https://github.com/ayooshkathuria/pytorch-yolo-v3)。
+[机器之心](https://www.jiqizhixin.com/users/7f316f0c-8f72-4231-bb30-0eb1dd5a5660)有两篇不错的中文翻译教程：[从零开始PyTorch项目：YOLO v3目标检测实现](https://www.jiqizhixin.com/articles/2018-04-23-3)，和[下篇](https://www.jiqizhixin.com/articles/042602)。github上源码[ayooshkathuria/pytorch-yolo-v3](https://github.com/ayooshkathuria/pytorch-yolo-v3)，作者正事基于英文教程，与教程的代码略有不同。
--- darknet.py
+++ darknet.py
@@ -30,12 +30,8 @@ def parse_cfg(cfgfile):
     blocks.append(block)
     return blocks
 
-class EmptyLayer(nn.Module):
-    def __init__(self):
-        super(EmptyLayer,self).__init__()
 
-
-def creat_modules(blocks):
+def create_modules(blocks):
     '''我们的函数将会返回一个 nn.ModuleList。这个类几乎等同于一个包含 nn.Module 对象的普通列表。
     然而，当添加 nn.ModuleList 作为 nn.Module 对象的一个成员时（即当我们添加模块到我们的网络时），
     所有 nn.ModuleList 内部的 nn.Module 对象（模块）的 parameter 也被添加作为 nn.Module 对象
@@ -54,7 +50,7 @@ def creat_modules(blocks):
 
     # 思路是迭代模块的列表，并为每个模块创建一个 PyTorch 模块
     for index, x in enumerate(blocks[1:]):
-        module = nn.Sequential  # 每个模块可能包含多个层，Sequential顺序串联起来
+        module = nn.Sequential()  # 每个模块可能包含多个层，Sequential顺序串联起来
 
         if (x["type"] == "convolutional"):
             activation = x["activation"]
@@ -71,53 +67,94 @@ def creat_modules(blocks):
             stride = int(x["stride"])  # 步长
 
             if padding:
-                pad = (kernel_size - 1) // 2 #整数除法
+                pad = (kernel_size - 1) // 2  # 整数除法
             else:
                 pad = 0
 
-            #卷积层
-            conv = nn.Conv2d(prev_filters,filters,kernel_size,stride,pad,bias=bias)
-            module.add_module("conv_{0}".format(index),conv)
+            # 卷积层
+            conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias=bias)
+            module.add_module("conv_{0}".format(index), conv)
 
-            #batch norm 层，标准化操作，训练时计算均值方差，验证时用方差验证
+            # batch norm 层，标准化操作，训练时计算均值方差，验证时用方差验证
             if batch_normalize:
                 bn = nn.BatchNorm2d(filters)
-                module.add_module("batch_norm_{0}".format(index),bn)
+                module.add_module("batch_norm_{0}".format(index), bn)
 
             if activation == "leaky":
-                acti = nn.LeakyReLU(0.1,inplace=True)   #负斜率0.1的relu，是否选择覆盖运算
-                module.add_module("leak_{0}".format(index),acti)
+                acti = nn.LeakyReLU(0.1, inplace=True)  # 负斜率0.1的relu，是否选择覆盖运算
+                module.add_module("leak_{0}".format(index), acti)
 
         # 上采样层，函数由interpolate替代，放缩倍数scale_factor和size只能给一个，扩展输入矩阵
         elif (x["type"] == "upsample"):
             stride = int(x["stride"])
             # 矩阵扩为2倍，最近邻nearest，线性linear，双线性bilinear 和 三线性trilinear
-            upsample = nn.Upsample(scale_factor=2,mode="bilinear")
-            module.add_module("upsample_{}".format(index),upsample)
+            upsample = nn.Upsample(scale_factor=2, mode="bilinear")
+            module.add_module("upsample_{}".format(index), upsample)
 
-        #路由层，有两种：1个数字，2个数字的
-        elif(x["type"] == "route"):
-            x["layer"] = x["layer"].split(',')
-            start = int(x["layer"][0])#第一个数字
+        # 路由层，获取之前层的拼接，有两种：1个数字，2个数字的
+        elif (x["type"] == "route"):
+            x["layers"] = x["layers"].split(',')
+            start = int(x["layers"][0])  # 第一个数字
             try:
-                end=int(x["layer"][1])#如果有第二个数字
+                end = int(x["layers"][1])  # 如果有第二个数字
             except:
-                end=0
+                end = 0
 
             if start > 0:
-                start = start-index
-            if end > 0 :
-                end = end-index
+                start = start - index
+            if end > 0:
+                end = end - index
 
-            route = EmptyLayer()    # 空层
-            module.add_module("route_{0}".format(index),route)
+            route = EmptyLayer()  # 空层，连接作用 保证前向传播
+            module.add_module("route_{0}".format(index), route)
 
-            if end<0:
-                filters=output_filters[index+start]+output_filters[index+end]
+            # 在路由层之后的卷积层会把它的卷积核应用到之前层的特征图（可能是拼接的）上。
+            # filters 变量以保存路由层输出的卷积核数量，即两层叠加
+            if end < 0:
+                filters = output_filters[index + start] + output_filters[index + end]
             else:
-                filters=output_filters[index+start]
+                filters = output_filters[index + start]
+
+        # 捷径层（跳过连接），将前一层的特征图添加到后面的层上
+        elif x["type"] == "shortcut":
+            shortcut = EmptyLayer()
+            module.add_module("shortcut_{}".format(index), shortcut)
+            # 捷径层也使用空的层，因为它还要执行一个非常简单的操作（加）。
+            # 没必要更新 filters 变量，因为它只是将前一层的特征图添加到后面的层上而已。
+
+        # yolo 检测层
+        elif x["type"] == "yolo":
+            mask = x["mask"].split(",")
+            mask = [int(x) for x in mask]
+
+            anchors = x["anchors"].split(",")  # 锚点框
+            anchors = [int(a) for a in anchors]
+            anchors = [(anchors[i], anchors[i + 1]) for i in range(0, len(anchors), 2)]
+            anchors = [anchors[i] for i in mask]  # 取mask中提到的几对锚点框
+
+            detection = DetectionLayer(anchors)
+            module.add_module("Detection_{}".format(index), detection)
+
+        # 统计
+        module_list.append(module)
+        prev_filters = filters
+        output_filters.append(filters)
+
+    #net_info就是net层信息，再返回各层list
+    return (net_info, module_list)
 
 
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer, self).__init__()
+        self.anchors = anchors
+
+# 测试
 # cfg = parse_cfg('yolov3.cfg')
-# creat_modules(cfg)
-# print(cfg)
+# [net,mod] = create_modules(cfg)
+# print(mod)
