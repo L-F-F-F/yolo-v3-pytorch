0d9c04aef72356398a0f23ca2c28801c673f4c43
L-F-F-F <33219996+L-F-F-F@users.noreply.github.com>
2018-11-06 11:48:58 +0800

    Update util.py

--- util.py
+++ util.py
@@ -13,7 +13,7 @@ import cv2
 def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA=True):
     batch_size = prediction.size(0)
     stride = inp_dim // prediction.size(2)
-    #grid_size = inp_dim // stride
+    # grid_size = inp_dim // stride
     grid_size = prediction.size(2)
     bbox_attrs = 5 + num_classes  # 每个边界框有5+C个属性：坐标tx ty tw th Obj 类别共C类
     num_anchors = len(anchors)
@@ -67,8 +67,140 @@ def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA=True):
     return prediction
 
 
-#输入为预测结果、置信度（objectness 分数阈值）、种类数 和 nms_conf（NMS IoU 阈值）
-def write_results(prediction, confidence, num_classes, nms_conf = 0.4):
-    #
-    conf_mask = (prediction[:, :, 4] > confidence).float().unsqueeze(2)
+# 输入为预测结果、置信度（objectness 分数阈值）、种类数 和 nms_conf（NMS IoU 阈值）
+def write_results(prediction, confidence, num_classes, nms_conf=0.4):
+    # prediction中 低于objectness分数的每个边界框，其每个属性值都置0，即一整行
+    conf_mask = (prediction[:, :, 4] > confidence).float().unsqueeze(2)  # 保留了大于置信度的部分
     prediction = prediction * conf_mask
+
+    # 框的中心坐标 转 左上角右下角，以计算IOU
+    box_corner = prediction.new(prediction.shape)
+    box_corner[:, :, 0] = (prediction[:, :, 0] - prediction[:, :, 2] / 2)
+    box_corner[:, :, 1] = (prediction[:, :, 1] - prediction[:, :, 3] / 2)
+    box_corner[:, :, 2] = (prediction[:, :, 0] + prediction[:, :, 2] / 2)
+    box_corner[:, :, 3] = (prediction[:, :, 1] + prediction[:, :, 3] / 2)
+    prediction[:, :, :4] = box_corner[:, :, :4]
+
+    batch_size = prediction.size(0)
+
+    write = False  # 标识尚未初始化输出
+
+    for ind in range(batch_size):
+        image_pred = prediction[ind]  # 每张图像 Tensor，[10647 x (5+类别数)]
+
+        # 获取最高类别分数 的分数 以及 索引
+        max_conf, max_conf_score = torch.max(image_pred[:, 5:5 + num_classes], 1)
+        max_conf = max_conf.float().unsqueeze(1)
+        max_conf_score = max_conf_score.float().unsqueeze(1)
+
+        # 不需要所有类别分数，仅保留最高分数的索引以及分数，替换 image_pred
+        seq = (image_pred[:, :5], max_conf, max_conf_score)
+        image_pred = torch.cat(seq, 1)  # cat 1 是列方向连接成tensor
+        # 所以[10647 x 7]
+
+        # 删除objectness置信度小于阈值的置0条目
+        non_zero_index = (torch.nonzero(image_pred[:, 4]))
+        try:  # 处理无检测结果的情况
+            image_pred_ = image_pred[non_zero_index.squeeze(), :].view(-1, 7)
+        except:
+            continue
+
+        if image_pred_.shape[0] == 0:  # PyTorch 0.4兼容
+            continue
+
+        # -1是 类别 的 index，获得一个图像的所有种类
+        img_classes = unique(image_pred_[:, -1])
+
+        # 按类别执行NMS
+        for cls in img_classes:
+            # 得到一个类别的所有检测
+            cls_mask = image_pred_ * (image_pred_[:, -1] == cls).float().unsqueeze(1)
+            class_mask_ind = torch.nonzero(cls_mask[:, -2]).squeeze()
+
+            image_pred_class = image_pred_[class_mask_ind].view(-1, 7)
+
+            # 对所有检测排序,按照objectness置信度
+            conf_sort_index = torch.sort(image_pred_class[:, 4], descending=True)[1]
+            image_pred_class = image_pred_class[conf_sort_index]
+            idx = image_pred_class.size(0)
+
+            # 对于每一个检测,执行NMS 非极大值抑制
+            for i in range(idx):
+
+                try:  # 当前box之后所有boxes的IOU
+                    ious = bbox_iou(image_pred_class[i].unsqueeze(0), image_pred_class[i + 1:])
+                except ValueError:  # image_pred_class[i+1,:]返回空张量
+                    break
+
+                except IndexError:  # image_pred_class移除部分后，idx索引越界
+                    break
+
+                # 清除IoU>阈值的检测
+                iou_mask = (ious < nms_conf).float().unsqueeze(1)
+                image_pred_class[i + 1:] *= iou_mask
+
+                # 移除0条目
+                non_zero_ind = torch.nonzero(image_pred_class[:, 4]).squeeze()
+                image_pred_class = image_pred_class[non_zero_ind].view(-1, 7)
+
+            #对图像中的类cls进行多次检测，重复batch_id
+            batch_ind = image_pred_class.new(image_pred_class.size(0), 1).fill_(ind)
+            seq = batch_ind, image_pred_class
+
+            #一旦其被初始化，就将后续的检测结果与它连接起来。
+            if not write:
+                output = torch.cat(seq, 1)
+                write = True
+            else:
+                out = torch.cat(seq, 1)
+                output = torch.cat((output, out))
+
+    #输出为 Dx8 的张量；其中 D 是所有图像中的「真实」检测结果，每个都用一行表示。
+    # 每一个检测结果都有 8 个属性，即：该检测结果所属的 batch 中图像的索引、
+    # 4 个角的坐标、objectness 分数、有最大置信度的类别的分数、该类别的索引。
+    try:
+        return output
+    except:
+        return 0
+
+
+# 因为同一类别可能会有多个「真实」检测结果,unique获取任意给定图像中存在的类别
+def unique(tensor):
+    tensor_np = tensor.cpu().numpy()
+    unique_np = np.unique(tensor_np)  # 去除重复数字,排序
+    unique_tensor = torch.from_numpy(unique_np)  # 转tensor
+
+    tensor_res = tensor.new(unique_tensor.shape)
+    tensor_res.copy_(unique_tensor)
+    return tensor_res
+
+
+# IOU计算 交并比
+def bbox_iou(box1, box2):
+    '''
+
+    :param box1:边界框行，这是由循环中的变量 i 索引
+    :param box2:多个边界框行构成的张量
+    :return:边界框与第二个输入中的每个边界框的 IoU
+    '''
+    # 边框的坐标
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:, 0], box1[:, 1], box1[:, 2], box1[:, 3]
+    b2_x1, b2_y1, b2_x2, b2_y2 = box2[:, 0], box2[:, 1], box2[:, 2], box2[:, 3]
+
+    # 获取交叉矩形的坐标
+    inter_rect_x1 = torch.max(b1_x1, b2_x1)
+    inter_rect_y1 = torch.max(b1_y1, b2_y1)
+    inter_rect_x2 = torch.min(b1_x2, b2_x2)
+    inter_rect_y2 = torch.min(b1_y2, b2_y2)
+
+    # 交集部分面积
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1,
+                                                                                     min=0)
+
+    # 并集部分面积
+    b1_area = (b1_x2 - b1_x1 + 1) * (b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1) * (b2_y2 - b2_y1 + 1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area)
+
+    return iou
